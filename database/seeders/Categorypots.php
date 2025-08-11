e059d647629f35d3943571db4f6ed913da79d802
rraysasalsabila <sraysasalsabila@gmail.com>
2025-08-11 10:50:57 +0700

    update

--- database/seeders/Categorypots.php
+++ database/seeders/Categorypots.php
@@ -2,7 +2,7 @@
 namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
-class CategorySeeder extends Seeder
+class Categorypots extends Seeder
 {
     /**
      * Run the database seeds.
